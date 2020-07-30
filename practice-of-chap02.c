1b939d8ae5658a71162503eaab52eef4e92e4959
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-07-30 03:39:17 -0700

    Update practice-of-chap02.c

--- practice-of-chap02.c
+++ practice-of-chap02.c
@@ -3,15 +3,6 @@
 int main()
 {
 
-    // int a;
-    // b = a;
-
-    // int v = 3 ^ 3;
-    // printf("%d ", v);
-
-    // char dt = "21 Dec 2020";
-
-    // Q3
 
     int num;
     printf("Enter A Number : ");
