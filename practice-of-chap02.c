ec32a5b9a929d59554477016b9083e8622c9e288
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-07-30 15:38:50 +0500

    Add files via upload

---
+++ practice-of-chap02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // int a;
+    // b = a;
+
+    // int v = 3 ^ 3;
+    // printf("%d ", v);
+
+    // char dt = "21 Dec 2020";
+
+    // Q3
+
+    int num;
+    printf("Enter A Number : ");
+    scanf("%d", &num);
+    printf("Divisibility Result Is : %d", num % 97);
+    return 0;
+}
